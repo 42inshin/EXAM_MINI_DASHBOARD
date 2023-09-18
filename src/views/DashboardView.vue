57b7b62cad9f432532c3a016e93f6f61a08393a8
42inshin <inshin@student.42seoul.kr>
2023-09-18 17:34:04 +0900

    [FE] FEAT: pie 차트 예시 코드 추가 #6

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import TheDashboard from '../components/TheDashboard.vue'
+import PieChart from '../components/PieChart.vue'
 </script>
 
 <template>
   <main>
     <TheDashboard />
+    <PieChart />
   </main>
 </template>
